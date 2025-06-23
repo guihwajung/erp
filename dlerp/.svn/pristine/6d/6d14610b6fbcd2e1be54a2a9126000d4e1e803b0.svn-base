182208b89b8058e5329e7168b32f71b321e34652
guihwajung <iness88@daum.net>
2025-06-23 16:01:15 +0900

    Delete dlerp directory

--- dlerp/.svn/pristine/6d/6d14610b6fbcd2e1be54a2a9126000d4e1e803b0.svn-base
+++
@@ -1,9 +0,0 @@
-<%@ page contentType="text/html;charset=utf-8" %>
-<html>
-<head>
-	<title>Page Test</title>
-</head>
-<body>
-	${msg}
-</body>
-</html>
--- dlerp/src/main/java/com/goodcen/cenerp/service/CENLogService.java
+++
@@ -1,7 +0,0 @@
-package com.goodcen.cenerp.service;
-
-import java.util.Map;
-
-public interface CENLogService {
-	public int insert(Map map) throws Exception;
-}
